459716d4cdf758ff3ae1915a822d1d05f3aa65b8
40923226 <40923226@gm.nfu.edu.tw>
2022-05-01 16:53:01 +0800

    w10 ssh wink

---
+++ downloads/11.js
@@ -0,0 +1,11 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "738": [
+      { type: "gotoframe", rect: { x: 385, y: 645, width: 60, height: 24 }, target: 739 },
+    ],
+  },
+};
